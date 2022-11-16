1c6bbe06c7d7293e90b03618a3e1420ba8856fe3
Agress2000 <117298339+Agress2000@users.noreply.github.com>
2022-11-16 18:35:03 +0300

    Add files via upload

---
+++ amity.java
@@ -0,0 +1,51 @@
+package amity;
+import java.util.spi.*;
+public class amity ;
+	
+	    public static void main(String args[])
+	    {
+	    String BCS2101;
+	    String BCS2102;
+	    String BCS2103;
+	    String BCS2104;
+	    String BCS2105;
+	    String BCS2106;
+	    int score=0;
+	       
+			System.out.println("Enter Marks for BCS2101:");
+			 BCS2101=in.nextline();
+	           if(BCS2101.equals("70")) {
+	        	   System.out.println("A");
+	           }else {
+	        	   System.out.println("u have failed to get A");
+	        	   
+	        	System.out.println("Enter Marks for BCS2102:");
+	       
+				 BCS2102=in.nextline();
+		           if(BCS2101.equals("70")) {
+		        	   System.out.println("A");
+		           }else {
+		        	   System.out.println("u have failed to get A");
+		        	   
+	           System.out.println("Enter Marks for BCS2103:");
+	     
+				 BCS2101=in.nextline();
+		           if(BCS2101.equals("70")) {
+		        	   System.out.println("A");
+		           }else {
+		        	   System.out.println("u have failed to get A");
+		        	   
+	           System.out.println("Enter Marks for BCS2104:");
+	           System.out.println("Enter Marks for BCS210:");
+	           System.out.println("Enter Marks for BCS2106:");
+	           	          
+	           
+	        }
+	    }
+	}
+
+
+
+
+
+
